ae05cc808a0deb789094ffc6486cd655180bb4d6
Aviv Gibli <avivgib1012@gmail.com>
2024-11-05 06:56:11 +0200

    Implement renderBooks function and remove hard-coded table rows

--- js/book.controller.js
+++ js/book.controller.js
@@ -5,4 +5,20 @@ function onInit() {
 }
 
 function renderBooks() {
+    const elBooks = document.querySelector('.books-container tbody')
+
+    const strHTMLs = gBooks.map(book =>
+        `<tr>
+                <th>${book.title}</th>
+                <th>${book.price}</th>
+                <th><img src="img/${book.imgUrl}" alt="${book.title} cover"></th>
+                <th class="actions">
+                    <button class="read">Read</button> 
+                    <button class="update">Update</button> 
+                    <button class="delete">Delete</button> 
+                </th>
+            </tr>`
+    )
+
+    elBooks.innerHTML = strHTMLs.join('')
 }
