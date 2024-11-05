689a45570a7de6c05d57e65f9234e7630e7a6ae7
Aviv Gibli <avivgib1012@gmail.com>
2024-11-05 05:42:01 +0200

    Initial project setup with HTML, service, and controller

---
+++ js/book.controller.js
@@ -0,0 +1,8 @@
+'use strict'
+
+function onInit() {
+    renderBooks()
+}
+
+function renderBooks() {
+}
---
+++ js/book.service.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const gBooks = [
+    { id: 'b101', title: 'Harry Potter and the Cursed Child', price: 88, imgUrl: 'harry-potter-and-the-cursed-child.jpg', author: 'J.K. Rowling' },
+    { id: 'b102', title: 'The Son of Neptune', price: 60, imgUrl: 'the-son-of-neptune.jpg', author: 'Rick Riordan' },
+    { id: 'b103', title: 'No Strangers Here', price: 64, imgUrl: 'no-strangers-here.jpg', author: 'Carlene O\'Connor' },
+    { id: 'b104', title: 'The Five', price: 75, imgUrl: 'the-five.jpg', author: 'Hallie Rubenhold' }
+]
+
+function getBooks() {
+    return gBooks
+}
